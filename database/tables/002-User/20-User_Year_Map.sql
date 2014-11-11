dec6cb7dc257497f608cf92c84491e457745c4d6
jdrydn <james@jdrydn.com>
2014-11-11 10:07:43 +0000

    Added a User to Year map, to connect users to a year. #kentprojects-59 fixed

---
+++ database/tables/002-User/20-User_Year_Map.sql
@@ -0,0 +1,12 @@
+/**
+ * @author: KentProjects <developer@kentprojects.com>
+ * @license: Copyright KentProjects
+ * @link: http://kentprojects.com
+ */
+CREATE TABLE IF NOT EXISTS `User_Year_Map` (
+	`user_id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+	`year` INT(4) UNSIGNED NOT NULL,
+	PRIMARY KEY (`user_id`, `year`),
+	FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (`year`) REFERENCES `Year` (`year`) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE = InnoDB CHARACTER SET utf8;
