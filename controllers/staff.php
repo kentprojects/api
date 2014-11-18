d295c4f404c8ac301f8e35f2df9db77a69ef0a58
jdrydn <james@jdrydn.com>
2014-11-18 17:10:54 +0000

    Here's a way to get a list of staff too. Because it was a copy and paste and a one-line change. #kentprojects-93 work development 5m

--- controllers/staff.php
+++ controllers/staff.php
@@ -12,30 +12,70 @@ final class Controller_Staff extends Controller
 	 */
 	public function action_index()
 	{
-		if (!in_array($this->request->getMethod(), array(Request::GET, Request::PUT)))
-		{
-			throw new HttpStatusException(501);
-		}
-
-		if ($this->request->param("id") === null)
-		{
-			throw new HttpStatusException(400, "No staff id provided.");
-		}
-
-		$user = Model_Staff::getById($this->request->param("id"));
-		if (empty($user))
-		{
-			throw new HttpStatusException(404, "Staff not found.");
-		}
-
-		if ($this->request->getMethod() === Request::PUT)
+		if ($this->request->param("id") !== null)
 		{
 			/**
-			 * Used to update staff!
+			 * /staff/:id
 			 */
+			$this->validateMethods(Request::GET, Request::PUT);
+
+			$user = Model_Staff::getById($this->request->param("id"));
+			if (empty($user))
+			{
+				throw new HttpStatusException(404, "Staff not found.");
+			}
+
+			if ($this->request->getMethod() === Request::PUT)
+			{
+				/**
+				 * Used to update staff!
+				 */
+			}
+
+			$this->response->status(200);
+			$this->response->body($user);
+			return;
+		}
+
+		/**
+		 * /staff
+		 */
+		$this->validateMethods(Request::GET);
+
+		/**
+		 * SELECT `user_id` FROM `User`
+		 * WHERE `role` = 'staff' AND `status` = 1
+		 */
+		$query = new Query_Builder("user_id", "User");
+		$query->where(array("field" => "role", "value" => "staff"));
+		$query->where(array("field" => "status", "value" => 1));
+
+		if ($this->request->query("year") !== null)
+		{
+			/**
+			 * JOIN `User_Year_Map` USING (`user_id`)
+			 * WHERE `User_Year_Map`.`year` = ?
+			 */
+			$query->join(array(
+				"table" => "User_Year_Map",
+				"how" => "USING",
+				"field" => "user_id"
+			));
+			$query->where(array(
+				"table" => "User_Year_Map",
+				"field" => "year",
+				"type" => "i",
+				"value" => $this->request->query("year")
+			));
+		}
+
+		$users = $query->execute()->singlevals();
+		foreach ($users as $k => $user_id)
+		{
+			$users[$k] = Model_User::getById($user_id);
 		}
 
 		$this->response->status(200);
-		$this->response->body($user);
+		$this->response->body($users);
 	}
 }
