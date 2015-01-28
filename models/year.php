6b576e3eb0c4ac44b771e88a0ee9ee12a8b97a6d
jdrydn <james@jdrydn.com>
2015-01-28 15:58:24 +0000

    Added a quick static method to work out the academic year.

--- models/year.php
+++ models/year.php
@@ -41,6 +41,20 @@ class Model_Year extends Model
 	}
 
 	/**
+	 * A year runs from September to August.
+	 * Therefore, if the month in the date is greater than or equal to September, return the year.
+	 * Otherwise, return the year minus one.
+	 *
+	 * @param string $date
+	 * @return Model_Year
+	 */
+	public static function getAcademicYearFromDate($date)
+	{
+		$date = strtotime($date);
+		return (intval(date("n", $date)) >= 9) ? intval(date("Y", $date)) : (intval(date("Y", $date)) - 1);
+	}
+
+	/**
 	 * @var int(4)
 	 */
 	protected $id;
