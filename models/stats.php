156763022d9b41bd767371c281450465bd94f990
jdrydn <james@jdrydn.com>
2015-02-09 12:24:57 +0000

    Added a new Stat.

--- models/stats.php
+++ models/stats.php
@@ -28,6 +28,11 @@ abstract class Model_Stats extends Model
 			"WHERE u.`role` = 'student' AND u.`status` = 1",
 			"AND uym.`year` = ?"
 		),
+		"total_groups_with_projects" => array(
+			"SELECT COUNT(g.`group_id`)", "FROM `Group` g",
+			"JOIN `Project` p USING (`group_id`)",
+			"AND g.`year` = ?"
+		),
 		"total_staff" => array(
 			"SELECT COUNT(u.`user_id`)", "FROM `User` u",
 			"JOIN `User_Year_Map` uym USING (`user_id`)",
