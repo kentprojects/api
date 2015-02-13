0a23769c13f89f791835de42688263f84b1abb48
jdrydn <james@jdrydn.com>
2015-02-13 12:29:00 +0000

    REALLY silly James.

---
+++ vendors/mail.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author: KentProjects <developer@kentprojects.com>
+ * @license: Copyright KentProjects
+ * @link: http://kentprojects.com
+ */
+
+require_once __DIR__ . "/phpmailer/class.phpmailer.php";
+require_once __DIR__ . "/phpmailer/class.pop3.php";
+require_once __DIR__ . "/phpmailer/class.smtp.php";
+
+final class Mail
+{
+
+}
