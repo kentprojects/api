196d60aff321b39762644c867c7727c566dc8a59
jdrydn <james@jdrydn.com>
2015-02-28 19:18:54 +0000

    Updated the deploy comments to reload the cache and the webserver. [ci-skip]

--- scripts/deploy_develop.sh
+++ scripts/deploy_develop.sh
@@ -5,6 +5,8 @@
 # @link: http://kentprojects.com
 #
 ssh kentprojects@kentprojects.com <<'ENDSSH'
-cd /var/www/kentprojects-api-dev && sudo -u www-data git pull
-php database/update.php
+cd /var/www/kentprojects-api-dev && sudo -u www-data git pull && \
+php database/update.php && \
+sudo service apache2 restart && \
+sudo service memcached restart
 ENDSSH
