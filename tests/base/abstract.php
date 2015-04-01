c37cd2cbf89bc26339515e8d8ec13b8664d8f770
jdrydn <james@jdrydn.com>
2015-04-01 17:15:06 +0100

    Comments for classes.

--- tests/base/abstract.php
+++ tests/base/abstract.php
@@ -3,10 +3,15 @@
  * @author: KentProjects <developer@kentprojects.com>
  * @license: Copyright KentProjects
  * @link: http://kentprojects.com
+ *
+ * Class KentProjects_TestBase
+ * This is a base test class for all KentProjects tests.
  */
 abstract class KentProjects_TestBase extends PHPUnit_Framework_TestCase
 {
 	/**
+	 * Asserts that a model is equal to another model.
+	 *
 	 * @param Model $expected
 	 * @param Model $actual
 	 * @param string $message
--- tests/base/model.php
+++ tests/base/model.php
@@ -3,6 +3,9 @@
  * @author: KentProjects <developer@kentprojects.com>
  * @license: Copyright KentProjects
  * @link: http://kentprojects.com
+ *
+ * Class KentProjects_Model_TestBase
+ * This is a base test class for all KentProjects tests on models.
  */
 abstract class KentProjects_Model_TestBase extends KentProjects_TestBase
 {
